52f73bbf42dc61abad93d173599c70a466e58e95
abdenassar01 <abdenassaramimi99@gmail.com>
2022-07-29 16:18:11 +0100

    adding navbar

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
+import { NavLink } from "@remix-run/react";
 
 function Navbar() {
   return (
-    <div className="nav">
-        Navbar
-    </div>
+    <header className="header">
+        <NavLink to="/" className="logo">
+            <img src="https://i.imgur.com/WzmrQug.png" alt="muslim app logo" className="logo-img" />
+            <h2 className="logo-header">Muslim</h2>
+        </NavLink>
+        <nav className="nav">
+            <ul className="links">
+            {
+                ['quran', 'azkar', 'sunnah', 'about'].map(item => (
+                    <li className="link-item" key={item}>
+                        <NavLink className="link" to={`/${item}`}>{item}</NavLink>
+                    </li>
+                ))
+            }
+            </ul>
+        </nav>
+    </header>
   )
 }
 
