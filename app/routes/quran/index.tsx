0e74b4040c200de33ad3b2af9db4748ca29b089c
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-15 18:20:39 +0100

    adding fitching logique

--- app/routes/quran/index.tsx
+++ app/routes/quran/index.tsx
@@ -1,63 +1,36 @@
+import { LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import axios from "axios";
 import Cart from "~/components/cart/Cart"
+import useFetch from "~/hooks/useFetch";
+import { Surah } from "~/types/Types";
+
+export const loader: LoaderFunction = async () => {
+
+  const res = await axios.get("https://raw.githubusercontent.com/semarketir/quranjson/master/source/surah.json");
+  
+  return res.data
+}; 
 
 function index() {
+
+  const data = useLoaderData();
+
   return (
     <div className="carts-wrappers">
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
-        <Cart nbr={1} name="Al-Fatiha" nameAr="الفاتحة" nameTr="The Opener" numberOfAyah={7} />
+      {
+        data?.map(
+          (surah: Surah) => 
+            <Cart 
+              key={ surah.index } 
+              nbr={ surah.index } 
+              name={ surah.title } 
+              nameAr={ surah.titleAr } 
+              nameTr={ surah.type } 
+              numberOfAyah={ surah.count } 
+            />
+          )
+      }  
     </div>
   )
 }
