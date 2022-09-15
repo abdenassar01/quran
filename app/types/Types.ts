f143577d317ccd3912741156e3875212206a81e2
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-15 18:20:02 +0100

    adding type file to refactor code

---
+++ app/types/Types.ts
@@ -0,0 +1,26 @@
+type Juz = {
+    index: string,
+    verse: {
+        start: string,
+        end: string
+    }
+}
+
+export type Surah = {
+    place: string,
+    type: string,
+    count: number,
+    title: string,
+    titleAr: string,
+    index: string,
+    pages: string,
+    juz: Juz[]
+} 
+
+export type CartProp = {
+    nbr: string,
+    name: string,
+    nameTr: string,
+    nameAr: string,
+    numberOfAyah: number
+  }
