371b7ebbb51a204f28be24fd020255a3caba0ca8
abdenassar01 <abdenassaramimi99@gmail.com>
2022-09-21 23:55:42 +0100

    adding azkar page

---
+++ app/routes/azkar.tsx
@@ -0,0 +1,21 @@
+import styles from "~/styles/azkar.css"
+
+export function links() {
+    return [
+      { 
+        rel: "stylesheet", 
+        href: styles
+      }
+    ];
+  }
+
+
+function azkar() {
+  return (
+    <div className="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, mollitia? Itaque corporis odit architecto corrupti quisquam labore eum inventore sed, sapiente minus autem quas. A est ratione veritatis assumenda optio.
+    </div>
+  )
+}
+
+export default azkar
